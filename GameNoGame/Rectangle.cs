4f8cfb0d140c7d6936b2561e07c0b0537410f443
SergeyPishchulov <lcmrb26@bk.ru>
2020-04-16 18:37:04 +0500

    Работа с Rectangle перенесена в его класс

--- GameNoGame/Rectangle.cs
+++ GameNoGame/Rectangle.cs
@@ -17,6 +17,31 @@ namespace GameNoGame
             Location = location;
             Size = size;
         }
+        public static bool AreIntersected(Rectangle r1, Rectangle r2)
+        {
+            var (r1Left, r1Right) = GetXBorders(r1);
+            var (r2Left, r2Right) = GetXBorders(r2);
+            var (r1Top, r1Bottom) = GetYBorders(r1);
+            var (r2Top, r2Bottom) = GetYBorders(r2);
+
+            return ProjectionIntersect(r1Left, r1Right, r2Left, r2Right)
+                && ProjectionIntersect(r1Top, r1Bottom, r2Top, r2Bottom);
+        }
+
+        public static (int, int) GetXBorders(Rectangle r)
+        {
+            return (r.Location.X - r.Size.Width / 2, r.Location.X + r.Size.Width / 2);
+        }
+
+        public static (int, int) GetYBorders(Rectangle r)
+        {
+            return (r.Location.Y - r.Size.Height / 2, r.Location.Y + r.Size.Height / 2);
+        }
+
+        static bool ProjectionIntersect(int minA, int maxA, int minB, int maxB)
+        {
+            return (minA <= minB) && (maxA >= minB) || (minB <= minA) && (maxB >= minA);
+        }
 
     }
 }
